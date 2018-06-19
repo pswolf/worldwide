189fd7fccb08fc0c6f0ba3eea258963c3f8e2f45
Patrick Wolf <pawo.mobil@gmail.com>
2018-06-19 17:47:37 +0200

    !Bilder-Ordner heißt jetzt pics!!! PHP-Code in mobile Version eingefügt

--- mobile/mobile_scroll.php
+++ mobile/mobile_scroll.php
@@ -1,9 +1,13 @@
+<?php
+  session_start();
+?>
 <!DOCTYPE html>
 <html lang="de" dir="ltr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/mobile_scroll.css">
+    <link rel="shortcut icon" href="../pics/fav-icon.ico">
     <script src="../script/jquery-3.3.1.min.js" charset="utf-8"></script>
     <script src="../script/scroll.js" charset="utf-8"></script>
     <script src="../script/jquery.mobile-events.min.js" charset="utf-8"></script>
@@ -13,16 +17,33 @@
   <body>
     <div id="container">
       <div id="logo">
-        <img src="../icons/logo.png" alt="W#RLDWIDE"/>
+        <img src="../pics/worldwide-white.png" alt="W#RLDWIDE"/>
       </div>
       <div id="scroll">
         <ul>
-          <li id="co"><h1>HashtaA</h1></li>
-          <li id="active"><h1>HashtaB</h1></li>
-          <li id="co"><h1>HashtaC</h1></li>
-          <li id="hidden"><h1>HashtaX</h1></li>
-          <li id="hidden"><h1>HashtaY</h1></li>
-          <li id="hidden"><h1>Leer</h1></li>
+          <?php
+          $pdo = new PDO("mysql:host=dd28600.kasserver.com;dbname=d02a5e56","d02a5e56","************");
+          $statement = $pdo->prepare("SHOW COLUMNS FROM data");
+          $statement->execute();
+          $result = $statement->fetchAll();
+          $pdo = null;
+          $j = 0;
+          for($i=3; $i<count($result);$i++){
+            $hashtaglist[$j] = $result[$i][0];
+            $j = $j+1;
+          }
+          for($i = 0; $i < count($hashtaglist) ; $i++){
+            if($i == 0 || $i == 2){
+              echo '<li id="co"><h1>'.$hashtaglist[$i].'</h1></li>';
+            }
+            if($i == 1){
+              echo '<li id="active"><h1>'.$hashtaglist[$i].'</h1></li>';
+            }
+            if($i >= 3){
+              echo '<li id="hidden"><h1>'.$hashtaglist[$i].'</h1></li>';
+            }
+          };
+          ?>
         </ul>
       </div>
       <div id="but1">
